7397b5111d1782f74a1ea9f0cf56a7308bbe3265
slottwo <khaled.phg+github@gmail.com>
2023-05-09 09:19:26 -0300

    program as name of executable file

--- run_mpi.sh
+++ run_mpi.sh
@@ -1,11 +1,11 @@
-mpicc -o $1 src/MPI/$1.c
+mpicc -o program src/MPI/$1.c
 
 N=3
 if [ $# -gt 1 ]; then
    N=$2
 fi
 
-if [ -f $1 ]; then
-   mpiexec -n $N ./$1
-   rm $1
+if [ -f program ]; then
+   mpiexec -n $N ./program
+   rm program
 fi
