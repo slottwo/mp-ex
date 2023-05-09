d4bf228483260e74cc14f3c425df3f7d7cbb0994
slottwo <khaled.phg+github@gmail.com>
2023-05-09 09:19:37 -0300

    Finalize 10

--- src/MPI/10.c
+++ src/MPI/10.c
@@ -19,14 +19,12 @@ int main()
     */
 
     static long n_step = 500000000;
-    double dx = 1 / (double)n_step;
+    double dx = 1.0 / (double)n_step;
 
     int i;
     double x, pi, sum = 0.;
 
-    double t_start = MPI_Wtime();
-
-    for (i = 0; i < n_step; i++)
+    for (i = rank; i < n_step; i += nprocs)
     {
         x = (i + 0.5) * dx;
         sum += 4.0 / (1.0 + x * x);
@@ -34,9 +32,17 @@ int main()
 
     pi = sum * dx;
 
-    printf("t: %.5fs\n", MPI_Wtime() - t_start);
+    double PI;
+    MPI_Reduce(&pi, &PI, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    printf("pi: %0.5f\n", pi);
+    // double t_start = MPI_Wtime();
+
+    // printf("t: %.5fs\n", MPI_Wtime() - t_start);
+
+    if (rank == 0)
+    {
+        printf("pi: %0.5f\n", PI);
+    }
 
     /*
     FIM
