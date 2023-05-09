6551545f6871868c7ba8e5730659888cb96f3d15
slottwo <khaled.phg+github@gmail.com>
2023-05-09 09:53:30 -0300

    ex10 time count

--- src/MPI/10.c
+++ src/MPI/10.c
@@ -24,6 +24,8 @@ int main()
     int i;
     double x, pi, sum = 0.;
 
+    double t_start = MPI_Wtime();
+
     for (i = rank; i < n_step; i += nprocs)
     {
         x = (i + 0.5) * dx;
@@ -32,16 +34,38 @@ int main()
 
     pi = sum * dx;
 
-    double PI;
+    double t_proc = MPI_Wtime() - t_start;
+    printf("t_%d: %.5fs\n", rank, t_proc);
+
+    double PI, t;
     MPI_Reduce(&pi, &PI, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-
-    // double t_start = MPI_Wtime();
-
-    // printf("t: %.5fs\n", MPI_Wtime() - t_start);
+    // MPI_Reduce(&t_proc, &t_total, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
 
     if (rank == 0)
     {
-        printf("pi: %0.5f\n", PI);
+        t = MPI_Wtime() - t_start;
+        printf("\nt: %.7fs\n", t);
+        printf("pi: %0.7f\n", PI);
+
+        // Serial
+
+        sum = 0;
+
+        t_start = MPI_Wtime();
+
+        for (i = 0; i < n_step; i++)
+        {
+            x = (i + 0.5) * dx;
+            sum += 4.0 / (1.0 + x * x);
+        }
+
+        pi = sum * dx;
+
+        double t_serial = MPI_Wtime() - t_start;
+        printf("\nt_s: %.5fs\n", t);
+        printf("pi: %0.7f\n", pi);
+        printf("speedup: %0.2f\n", t_serial / t);
+        printf("eff.: %.5f\n", t_serial / t / nprocs);
     }
 
     /*
