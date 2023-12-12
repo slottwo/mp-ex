851d6e7da3128b3df8d2a13ae98b4111fbc01405
slottwo <41028091+slottwo@users.noreply.github.com>
2023-12-12 08:56:03 -0300

    Tamplate

---
+++ src/MP/0.c
@@ -0,0 +1,25 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char const *argv[])
+{
+    int NTHREADS = omp_get_num_procs() / 2;
+    
+    double t_start, t_serial, t_parallel;
+
+    // Serial
+
+    t_start = omp_get_wtime();
+
+    t_serial = omp_get_wtime() - t_start;
+
+    // Parallel
+
+    t_start = omp_get_wtime();
+
+    t_parallel = omp_get_wtime() - t_start;
+
+    
+}
