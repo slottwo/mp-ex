6f85cb20f94ff1fbf615c2c0f67f346b8d5d83db
slottwo <41028091+slottwo@users.noreply.github.com>
2023-12-14 10:11:50 -0300

    Remove global size

--- lib/generators.c
+++ lib/generators.c
@@ -4,30 +4,34 @@
 #include <stdlib.h>
 #include "headers/generators.h"
 
-void gen_vector_zto(double *v)
+void gen_vector_zto(double *v, int size)
 {
     time_t t;
     srand((unsigned)time(&t));
+    
+    int NTHREADS = omp_get_num_procs() / 2;
 
-#pragma omp parallel num_threads(3)
+#pragma omp parallel num_threads(NTHREADS)
     {
 #pragma omp for
-        for (int i = 0; i < SIZE; i++)
+        for (int i = 0; i < size; i++)
         {
             v[i] = (rand() / (double)RAND_MAX);
         }
     }
 }
 
-void gen_vector_int(int *v, int min, int max)
+void gen_vector_int(int *v, int min, int max, int size)
 {
     time_t t;
     srand((unsigned)time(&t));
 
-#pragma omp parallel num_threads(3)
+    int NTHREADS = omp_get_num_procs() / 2;
+
+#pragma omp parallel num_threads(NTHREADS)
     {
 #pragma omp for
-        for (int i = 0; i < SIZE; i++)
+        for (int i = 0; i < size; i++)
         {
             v[i] = (rand() % max + min);
         }
@@ -41,6 +45,7 @@ int gen_int(int min, int max)
 
 void statistic_log(double t, double t_serial, int nthreads)
 {
+    printf("\n");
     printf("serial:    %.3fµs\n", t_serial * 1000000);
     printf("parallel:  %.3fµs\n", t * 1000000);
 
--- lib/headers/generators.h
+++ lib/headers/generators.h
@@ -1,6 +1,4 @@
-#define SIZE 5000000
-
-void gen_vector_zto(double *);
-void gen_vector_int(int *, int, int);
+void gen_vector_zto(double *, int);
+void gen_vector_int(int *, int, int, int);
 int gen_int(int, int);
 void statistic_log(double, double, int);
