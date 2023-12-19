8813f63ed52eff04771696f764fc805c171d7452
slottwo <41028091+slottwo@users.noreply.github.com>
2023-12-19 09:36:49 -0300

    Parralel PI (mp/3)

--- src/MP/3.c
+++ src/MP/3.c
@@ -4,13 +4,15 @@
 #include <time.h>
 #include "../../lib/headers/generators.h"
 
-#define N 10000000
+#define N 300000000
 
 int main(int argc, char const *argv[])
 {
     int NTHREADS = omp_get_num_procs() / 2;
 
     double t_start, t_serial, t_parallel, x, dx, pi, sum = 0;
+
+    printf("PI: 3.14159265358979323846264338327950288\n");
 
     // Serial
 
@@ -28,16 +30,28 @@ int main(int argc, char const *argv[])
 
     t_serial = omp_get_wtime() - t_start;
 
-    printf("PI: 3.14159265358979323846264338327950288\n");
     printf("pi: %.35f\n", pi);
 
     // Parallel
 
+    sum = 0;
+
     t_start = omp_get_wtime();
 
-#pragma omp parallel num_threads(NTHREADS)
+#pragma omp parallel num_threads(NTHREADS) private(x)
+    {
+#pragma omp for reduction(+ : sum)
+        for (int i = 0; i < N; i++)
+        {
+            x = (i + 0.5) * dx;
+            sum += 4.0 / (1.0 + x * x);
+        }
+    }
+    pi = sum * dx;
 
     t_parallel = omp_get_wtime() - t_start;
+
+    printf("pi: %.35f\n", pi);
 
     statistic_log(t_serial, t_parallel, NTHREADS);
 
