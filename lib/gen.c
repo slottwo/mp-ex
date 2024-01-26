351d0f1a9003fd084f68318e787c26cdc6d381f6
Pedro Galvão <41028091+slottwo@users.noreply.github.com>
2024-01-26 12:01:52 -0300

    gen lib init exception handling, and lib docstring

--- lib/gen.c
+++ lib/gen.c
@@ -5,13 +5,35 @@
 
 #include "gen.h"
 
+int __GEN_INITIALIZED__ = 0;
+
+/**
+ * @brief Init random seed (should be called only once)
+ *
+ */
 void gen_init()
 {
-    time_t t;
-    srand((unsigned)time(&t));
+    if (__GEN_INITIALIZED__ == 0)
+    {
+        time_t t;
+        srand((unsigned)time(&t));
+        __GEN_INITIALIZED__ = 1;
+    }
+    else
+    {
+        printf("GEN_C Error: srand is already initalized\n");
+    }
 }
 
-void gen_vector(double *v, double min, double max, int size)
+/**
+ * @brief Fill a vector with random floating point values
+ *
+ * @param v Allocated vector
+ * @param size Vector length
+ * @param min Minimum value
+ * @param max Maximum value
+ */
+void gen_vector(double *v, int size, double min, double max)
 {
     int NTHREADS = omp_get_num_procs() / 2;
 
@@ -25,6 +47,12 @@ void gen_vector(double *v, double min, double max, int size)
     }
 }
 
+/**
+ * @brief Fill a vector with random floating point values ​​between `0` and `1`
+ *
+ * @param v Allocated vector
+ * @param size Vector length
+ */
 void gen_vector_zto(double *v, int size)
 {
     int NTHREADS = omp_get_num_procs() / 2;
@@ -39,7 +67,15 @@ void gen_vector_zto(double *v, int size)
     }
 }
 
-void gen_vector_int(int *v, int min, int max, int size)
+/**
+ * @brief Fill a vector with random integer values
+ *
+ * @param v Allocated vector
+ * @param size Vector length
+ * @param min Minimum value
+ * @param max Maximum value
+ */
+void gen_vector_int(int *v, int size, int min, int max)
 {
     int NTHREADS = omp_get_num_procs() / 2;
 
@@ -53,6 +89,13 @@ void gen_vector_int(int *v, int min, int max, int size)
     }
 }
 
+/**
+ * @brief Gen a random integer value
+ * 
+ * @param min Minimum value
+ * @param max Maximum value
+ * @return `int` – Generated value
+ */
 int gen_int(int min, int max)
 {
     if (max == RAND_MAX)
@@ -61,6 +104,12 @@ int gen_int(int min, int max)
     return rand() % (max - min + 1) + min;
 }
 
+/**
+ * @brief Gen a random floating point between `0` and `max`
+ * 
+ * @param max Maximum value
+ * @return `double` – Generated value
+ */
 double gen_rand(double max)
 {
     return (rand() / (double)RAND_MAX) * max;
--- lib/gen.h
+++ lib/gen.h
@@ -1,10 +1,12 @@
 #ifndef GEN_H
 #define GEN_H
 
+extern int __GEN_INITIALIZED__;
+
 void gen_init();
 double gen_rand(double);
 int gen_int(int, int);
-void gen_vector(double *, double, double, int);
+void gen_vector(double *, int, double, double);
 void gen_vector_zto(double *, int);
 void gen_vector_int(int *, int, int, int);
 void statistic_log(double, double, int);
