99e468475856211c7676b5f50e5d5cf5c81d2949
slottwo <41028091+slottwo@users.noreply.github.com>
2024-02-27 09:51:40 -0300

    Ex 11

---
+++ src/MPI/2.c
@@ -0,0 +1,108 @@
+/**
+ * @file 0.c
+ * @author slottwo (41028091+slottwo@users.noreply.github.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-02-22
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 1000
+
+/**
+ * @brief Returns a array of random values between 0 and 1
+ *
+ * @param size
+ * @return double*
+ */
+double *gen(int size);
+
+int main()
+{
+    // Iniciando MPI.h
+    int nprocs, rank;
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Status status;
+
+    // Iniciando Time.h
+    srand(time(NULL) * rank);
+
+    double t_init;
+    if (rank == 0)
+        t_init = MPI_Wtime();
+
+    /*
+    INICIO
+    */
+
+    double *v, s = 0;
+    if (rank == 0)
+        v = gen(N);
+
+    MPI_Bcast(v, N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    switch (rank)
+    {
+    case 0:
+        MPI_Recv(&s, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("1: %f\n", s);
+
+        MPI_Recv(&s, 1, MPI_DOUBLE, 2, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("2: %f\n", s);
+
+        break;
+
+    case 1:
+        for (int i = 0; i < N; i++)
+            s += v[i];
+
+        MPI_Send(&s, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+        break;
+
+    case 2:
+        for (int i = 0; i < N; i++)
+            s += v[i];
+
+        s *= 2;
+        MPI_Send(&s, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+        break;
+
+    default:
+        break;
+    }
+
+    /*
+    FIM
+    */
+
+    if (rank == 0)
+        printf("Δt: %f\n", MPI_Wtime() - t_init);
+
+    free(v);
+
+    MPI_Finalize();
+    return 0;
+}
+
+double *gen(int size)
+{
+    double *v;
+    v = (double *)malloc(sizeof(double) * size);
+
+    for (int i = 0; i < size; i++)
+    {
+        double num = (rand() / (double)RAND_MAX);
+        v[i] = num;
+    }
+
+    return v;
+}
